d274763b17fead37b073f98248bfc85c329a8fef
enochdev2 <enochpromise.va@gmail.com>
2025-08-28 13:27:52 +0100

    adding functionality

--- src/pages/PointExchange.jsx
+++ src/pages/PointExchange.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const PointExchange = () => {
   return (
-    <div>
+    <div className='bg-black min-h-screen text-white flex items-center justify-center'>
       
     </div>
   );
